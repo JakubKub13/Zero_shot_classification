c85cb8d7af368c84318407485933fc0387527219
JakubKub13 <jakubkubala3@gmail.com>
2023-01-06 12:32:35 +0100

    Creating pandas dataFrame

--- zero_shot_classification.py
+++ zero_shot_classification.py
@@ -14,3 +14,12 @@ from sklearn.metrics import roc_auc_score, f1_score, confusion_matrix
 classifier = pipeline("zero-shot-classification", device=0)
 
 classifier("This is a great movie", candidate_labels=["positive", "negative"])
+
+text = "Due to the presence of isoforms of its components, there are 12 versions of AMPK in mammals, each of which can have different tissue localizations, and different functions under different conditions. AMPK is ragulated allosterically and by post-translational modification, which work together"
+classifier(text, candidate_labels=["biology", "math", "geology"])
+
+data_frame = pd.read_csv('bbc_text_cls.csv')
+len(data_frame)
+
+data_frame.sample(frac=1).head()
+
