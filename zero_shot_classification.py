208040430f858917ae55fd828169ae3301e183d2
JakubKub13 <jakubkubala3@gmail.com>
2023-01-06 12:31:21 +0100

    Testing classifier

--- zero_shot_classification.py
+++ zero_shot_classification.py
@@ -12,3 +12,5 @@ import textwrap
 from sklearn.metrics import roc_auc_score, f1_score, confusion_matrix
 
 classifier = pipeline("zero-shot-classification", device=0)
+
+classifier("This is a great movie", candidate_labels=["positive", "negative"])
